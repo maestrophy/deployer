6309c89ab77eddcad9916564a8d80abd6502e682
Ferenczi Viktor <ferenczi.viktor92@gmail.com>
2025-03-28 11:25:08 +0100

    Fixed path formatting

--- Services/ProjectService.php
+++ Services/ProjectService.php
@@ -100,7 +100,7 @@ class ProjectService {
 			throw new Exception('Project path is not defined!', 100002);
 		}
 
-		PathUtil::makeFullPathFromRelative($projectData['projectPath']);
+		$projectData['projectPath'] = PathUtil::makeFullPathFromRelative($projectData['projectPath']);
 		self::$logger->info('Project path', $projectData['projectPath']);
 		exec("git -C " . escapeshellarg($projectData['projectPath']) . " rev-parse --is-inside-work-tree 2>/dev/null", $output, $isGitRepo);
 		self::$logger->info('Project path', $projectData['projectPath']);
