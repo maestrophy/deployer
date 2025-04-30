991b6c8a9c0e5e4d0425c599129994b3980c7c0c
Ferenczi Viktor <ferenczi.viktor92@gmail.com>
2025-04-30 15:36:57 +0200

    Trying to fix command executing

--- Services/CommandService.php
+++ Services/CommandService.php
@@ -11,8 +11,8 @@ class CommandService {
 		$userConfig = include 'config.php';
 		foreach ($commands as $command) {
 			self::$logger->info('Command', $command);
-			self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c '" . $command . "'");
-			$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c '" . $command . "'", $output, $exitCode);
+			self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c '" . self::formatCommand($command) . "'");
+			$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c '" . self::formatCommand($command) . "'", $output, $exitCode);
 			self::$logger->info('Output', $output);
 			self::$logger->info('Exit code', $exitCode);
 			self::$logger->info('Result', $result);
@@ -28,8 +28,8 @@ class CommandService {
 		self::initLogger();
 		$userConfig = include 'config.php';
 		self::$logger->info('Command', $command);
-		self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c '" . $command . "'");
-		$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c '" . $command . "'", $output, $exitCode);
+		self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c '" . self::formatCommand($command) . "'");
+		$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c '" . self::formatCommand($command) . "'", $output, $exitCode);
 		self::$logger->info('Output', $output);
 		self::$logger->info('Exit code', $exitCode);
 		self::$logger->info('Result', $result);
@@ -45,8 +45,8 @@ class CommandService {
 		$userConfig = include 'config.php';
 		foreach ($commands as $command) {
 			self::$logger->info('Command', $command);
-			self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . $command . "'");
-			$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . $command . "'", $output, $exitCode);
+			self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . self::formatCommand($command) . "'");
+			$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . self::formatCommand($command) . "'", $output, $exitCode);
 			self::$logger->info('Output', $output);
 			self::$logger->info('Exit code', $exitCode);
 			self::$logger->info('Result', $result);
@@ -62,8 +62,8 @@ class CommandService {
 		self::initLogger();
 		$userConfig = include 'config.php';
 		self::$logger->info('Command', $command);
-		self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . $command . "'");
-		$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . $command . "'", $output, $exitCode);
+		self::$logger->info('Full command', "echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . self::formatCommand($command) . "'");
+		$result = exec("echo '" . $userConfig['password'] . "' | su - maestro -c 'cd " . $path . " && " . self::formatCommand($command) . "'", $output, $exitCode);
 		self::$logger->info('Output', $output);
 		self::$logger->info('Exit code', $exitCode);
 		self::$logger->info('Result', $result);
@@ -83,5 +83,14 @@ class CommandService {
 		} else {
 			self::$logger = $logger;
 		}
+	}
+
+	private static function formatCommand(string $command): string
+	{
+		$result = htmlspecialchars_decode($command);
+		if (preg_match('/docker/', $result) && preg_match('/ -it /', $result)) {
+			$result = preg_replace('/ -it /', ' -i ', $result);
+		}
+		return $result;
 	}
 }
