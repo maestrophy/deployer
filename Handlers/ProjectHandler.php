c0146282f16da615112006d914fa3320a945a2b7
Ferenczi Viktor <ferenczi.viktor92@gmail.com>
2025-05-15 07:21:55 +0200

    Trying to fix display error

--- Handlers/ProjectHandler.php
+++ Handlers/ProjectHandler.php
@@ -95,6 +95,8 @@ class ProjectHandler extends AbstractHandler {
 	 */
 	public function changeScript(string $projectName)
 	{
+		$logger = new Logger('', __class__);
+		$logger->setLogLevel();
 		/**
 		 * @var ProjectService
 		 */
